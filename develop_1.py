0bdef374acce94ca22d5949b12448fbada60cf32
test_python <khazaei.amir110@gmail.com>
2022-08-21 19:39:53 +0430

    Test_Add_GitHub

--- develop_1.py
+++ develop_1.py
@@ -1,3 +1,18 @@
 print("hello develop_1")
 a = "develop_1"
 print("add " + a)
+
+
+class AddProjectGitHub:
+    def __int__(self):
+        pass
+
+    def __str__(self):
+        pass
+
+    def __new__(cls, *args, **kwargs):
+        pass
+
+    def __add__(self, other):
+        pass
+    
